b559fcb2f852a8a8857efb8e3cbe21ec12dabddb
Yeh9786 <yehyob9786@gmail.com>
2023-05-15 11:27:13 +0800

    renew image nft

--- sample.js
+++ sample.js
@@ -21,16 +21,16 @@ AFRAME.registerComponent('modify-materials', {
 
         // Wait for model to load.
         this.el.addEventListener('model-loaded', () => {
-            // Grab the mesh / scene.
-            const obj = this.el.getObject3D('mesh');
-            // Go over the submeshes and modify materials we want.
-            obj.traverse(node => {
-                console.log("node.name:", node.name);
-                if (node.name == "LeePerrySmith") {
-                    // node.material.color.set('red');
-                    node.material.map = tex;
-                }
-            });
+            // // Grab the mesh / scene.
+            // const obj = this.el.getObject3D('mesh');
+            // // Go over the submeshes and modify materials we want.
+            // obj.traverse(node => {
+            //     console.log("node.name:", node.name);
+            //     if (node.name == "LeePerrySmith") {
+            //         // node.material.color.set('red');
+            //         node.material.map = tex;
+            //     }
+            // });
         });
     }
 });
